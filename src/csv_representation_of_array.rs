f276fc9e3d347998e43fbe746519c5e97b30bf06
Jay T <jaytula@gmail.com>
2022-11-30 08:42:38 -0800

    Solve differently with map collect::<Vec<_>>() join

--- src/csv_representation_of_array.rs
+++ src/csv_representation_of_array.rs
@@ -1,25 +1,26 @@
 // Solutions: https://www.codewars.com/kata/5a34af40e1ce0eb1f5000036/solutions/rust
 
 fn to_csv_text(arr: &[Vec<i8>]) -> String {
-    let mut all = vec![];
-    for i in arr.iter() {
-        all.push(join_with_comma(i.to_vec()));
-    }
-
-    return all.join("\n");
-
-    // return String::from("abc");
+    arr.iter()
+        .map(|row| {
+            row.iter()
+                .map(|x| x.to_string())
+                .collect::<Vec<_>>()
+                .join(",")
+        })
+        .collect::<Vec<_>>()
+        .join("\n")
 }
 
-fn join_with_comma(v: Vec<i8>) -> String {
-    let mut vs = vec![];
-    for i in v.iter() {
-        // println!("i: {}", i);
-        vs.push(i.to_string());
-    }
-    let s = vs.join(",");
-    return s;
-}
+// fn join_with_comma(v: Vec<i8>) -> String {
+//     let mut vs = vec![];
+//     for i in v.iter() {
+//         // println!("i: {}", i);
+//         vs.push(i.to_string());
+//     }
+//     let s = vs.join(",");
+//     return s;
+// }
 // Add your tests here.
 // See https://doc.rust-lang.org/stable/rust-by-example/testing/unit_testing.html
 
@@ -27,11 +28,13 @@ fn join_with_comma(v: Vec<i8>) -> String {
 mod tests {
     use super::to_csv_text;
     // use super::join_with_comma;
-    
+
     fn do_test(input: &[Vec<i8>], expected: &str) {
-        let actual  = to_csv_text(input);
-        assert!(actual == expected,
-            "Test failed with array = {input:?}\nExpected \"{expected}\"\nbut got \"{actual}\"");
+        let actual = to_csv_text(input);
+        assert!(
+            actual == expected,
+            "Test failed with array = {input:?}\nExpected \"{expected}\"\nbut got \"{actual}\""
+        );
     }
 
     #[test]
@@ -41,21 +44,27 @@ mod tests {
         // assert!(actual == expected, "Yay");
 
         for (input, expected) in [
-            (vec![
-            vec![0, 1, 2, 3, 45],
-            vec![10, 11, 12, 13, 14],
-            vec![20, 21, 22, 23, 24],
-            vec![30, 31, 32, 33, 34]
-        ], "0,1,2,3,45\n10,11,12,13,14\n20,21,22,23,24\n30,31,32,33,34"),
-            (vec![
-            vec![-25, 21, 2, -33, 48],
-            vec![30, 31, -32, 33, -34]
-        ], "-25,21,2,-33,48\n30,31,-32,33,-34"),
-            (vec![
-            vec![5, 55, 5, 5, 55],
-            vec![6, 6, 66, 23, 24],
-            vec![127, 31, 66, 33, 7]
-        ], "5,55,5,5,55\n6,6,66,23,24\n127,31,66,33,7")
+            (
+                vec![
+                    vec![0, 1, 2, 3, 45],
+                    vec![10, 11, 12, 13, 14],
+                    vec![20, 21, 22, 23, 24],
+                    vec![30, 31, 32, 33, 34],
+                ],
+                "0,1,2,3,45\n10,11,12,13,14\n20,21,22,23,24\n30,31,32,33,34",
+            ),
+            (
+                vec![vec![-25, 21, 2, -33, 48], vec![30, 31, -32, 33, -34]],
+                "-25,21,2,-33,48\n30,31,-32,33,-34",
+            ),
+            (
+                vec![
+                    vec![5, 55, 5, 5, 55],
+                    vec![6, 6, 66, 23, 24],
+                    vec![127, 31, 66, 33, 7],
+                ],
+                "5,55,5,5,55\n6,6,66,23,24\n127,31,66,33,7",
+            ),
         ] {
             do_test(&input, expected)
         }
