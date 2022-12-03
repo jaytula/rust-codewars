1de5aafd58207e33738dd73570a863c78279efd5
Jay T <jaytula@gmail.com>
2022-12-02 16:40:39 -0800

    Complete 'Calculate BMI'

---
+++ src/calculate_bmi.rs
@@ -0,0 +1,29 @@
+// https://www.codewars.com/kata/57a429e253ba3381850000fb/solutions/rust
+
+fn bmi(weight: u32, height: f32) -> &'static str {
+    let bmi = weight as f32 / height.powf(2.0);
+
+    if bmi <= 18.5 {
+        return "Underweight";
+    };
+    if bmi <= 25.0 {
+        return "Normal";
+    };
+    if bmi <= 30.0 {
+        return "Overweight";
+    };
+    "Obese"
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn basic_tests() {
+        assert_eq!(bmi(50, 1.80), "Underweight");
+        assert_eq!(bmi(80, 1.80), "Normal");
+        assert_eq!(bmi(90, 1.80), "Overweight");
+        assert_eq!(bmi(110, 1.80), "Obese");
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,7 @@ mod parse_nice_int_from_char;
 mod htmlspecialchars;
 mod difference_of_volumes_of_cuboids;
 mod third_angle_of_a_triangle;
+mod calculate_bmi;
 
 #[cfg(test)]
 
