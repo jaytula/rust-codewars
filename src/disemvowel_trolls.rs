7ce5e150ec14899cb24153ec8f8cfcb9654942dd
Jay T <jaytula@gmail.com>
2022-12-20 12:15:37 -0800

    Alternative 1 for Disemvowel trolls

--- src/disemvowel_trolls.rs
+++ src/disemvowel_trolls.rs
@@ -1,24 +1,22 @@
 // https://www.codewars.com/kata/52fba66badcd10859f00097e/solutions/rust
 
 fn disemvowel(s: &str) -> String {
-    s.replace("a", "")
-    .replace("e", "")
-    .replace("i", "")
-    .replace("o", "")
-    .replace("u", "")
-    .replace("A", "")
-    .replace("E", "")
-    .replace("I", "")
-    .replace("O", "")
-    .replace("U", "")
+    let res = s
+        .chars()
+        .filter(|ch| !"aeiou".contains(ch.to_ascii_lowercase()))
+        .collect();
+    res
 }
 
 #[cfg(test)]
 mod tests {
     use super::disemvowel;
-    
+
     #[test]
     fn example_test() {
-        assert_eq!(disemvowel("This website is for losers LOL!"), "Ths wbst s fr lsrs LL!");
+        assert_eq!(
+            disemvowel("This website is for losers LOL!"),
+            "Ths wbst s fr lsrs LL!"
+        );
     }
 }
