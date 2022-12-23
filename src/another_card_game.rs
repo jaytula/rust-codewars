280b48bc510a39faefb46c307f2a37662ecb6eb0
Jay T <jaytula@gmail.com>
2022-12-23 11:58:46 -0800

    Add weird_day_5 failing case

--- src/another_card_game.rs
+++ src/another_card_game.rs
@@ -83,6 +83,11 @@ mod tests {
     }
 
     #[test]
+    fn weird_day_5() {
+        assert_eq!(the_game(&[1, 3, 4, 9], &[0, 5, 6, 7], &[2, 8, 10, 11]), true)
+    }
+
+    #[test]
     fn good_day() {
         assert_eq!(the_game(&[2, 5, 8, 11], &[1, 4, 7, 10], &[0, 3, 6, 9]), true);
     }
