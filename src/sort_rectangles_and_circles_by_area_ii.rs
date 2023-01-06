faee850a31bc627391b1a1279eb82981efd48bcc
Jay T <jaytula@gmail.com>
2023-01-05 18:38:04 -0800

    Complete 'Sort rectangles and circles by area II'

--- src/sort_rectangles_and_circles_by_area_ii.rs
+++ src/sort_rectangles_and_circles_by_area_ii.rs
@@ -1,38 +1,95 @@
-use std::{cmp::Ordering, f64::consts::PI};
+// https://www.codewars.com/kata/5a1ebc2480171f29cf0000e5/solutions/rust
+
 use either::Either;
+use itertools::Itertools;
+use std::{cmp::Ordering, f64::consts::PI};
 
 struct MyEither(Either<(f64, f64), f64>);
 
 impl Ord for MyEither {
     fn cmp(&self, other: &Self) -> Ordering {
-        let mut num1: f64 =  0.0;
-        if self.is_left() {
-            let (l, w) = self.left().unwrap();
-            num1 = l * w;
+        let num1 = if self.0.is_left() {
+            let (l, w) = self.0.left().unwrap();
+            l * w
         } else {
-            let r = self.right().unwrap();
-            num1 = PI * r.powf(2);
-        }
-        let mut num2: f64 = 0.0;
-        if other.is_left() {
-            let (l, w) = other.left().unwrap();
-            num2 = l * w;
+            let r = self.0.right().unwrap();
+            PI * r.powf(2.0)
+        };
+        let num2 = if other.0.is_left() {
+            let (l, w) = other.0.left().unwrap();
+            l * w
         } else {
-            let r = other.right().unwrap();
-            num2 = PI * r.powf(2);
-        }
+            let r = other.0.right().unwrap();
+            PI * r.powf(2.0)
+        };
         if num1 == num2 {
             return Ordering::Equal;
         }
-        if num1 < num2 { Ordering::Less } else { Ordering::Greater }
+        if num1 < num2 {
+            Ordering::Less
+        } else {
+            Ordering::Greater
+        }
     }
 }
 
+impl PartialOrd for MyEither {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        let num1 = if self.0.is_left() {
+            let (l, w) = self.0.left().unwrap();
+            l * w
+        } else {
+            let r = self.0.right().unwrap();
+            PI * r.powf(2.0)
+        };
+        let num2 = if other.0.is_left() {
+            let (l, w) = other.0.left().unwrap();
+            l * w
+        } else {
+            let r = other.0.right().unwrap();
+            PI * r.powf(2.0)
+        };
+        if num1 == num2 {
+            return Some(Ordering::Equal);
+        }
+        if num1 < num2 {
+            Some(Ordering::Less)
+        } else {
+            Some(Ordering::Greater)
+        }
+    }
+}
+
+impl PartialEq for MyEither {
+    fn eq(&self, other: &Self) -> bool {
+        let num1 = if self.0.is_left() {
+            let (l, w) = self.0.left().unwrap();
+            l * w
+        } else {
+            let r = self.0.right().unwrap();
+            PI * r.powf(2.0)
+        };
+        let num2 = if other.0.is_left() {
+            let (l, w) = other.0.left().unwrap();
+            l * w
+        } else {
+            let r = other.0.right().unwrap();
+            PI * r.powf(2.0)
+        };
+        num1 == num2
+    }
+}
+impl Eq for MyEither {}
+
 fn sort_by_area(_seq: &[Either<(f64, f64), f64>]) -> Vec<Either<(f64, f64), f64>> {
-    let mut res = _seq.to_vec();
+    let mut res = _seq
+        .to_vec()
+        .iter()
+        .map(|el| MyEither(*el))
+        .collect::<Vec<_>>();
     res.sort();
 
-    res
+    res.iter().map(|el| el.0).collect_vec()
     // [Either::Left((1.342 as f64, 3.212 as f64)), Either::Right(1.23), Either::Left((4.23, 6.43)), Either::Right(3.444)].to_vec()
 }
 
@@ -43,16 +100,32 @@ fn sort_by_area(_seq: &[Either<(f64, f64), f64>]) -> Vec<Either<(f64, f64), f64>
 mod tests {
     use super::sort_by_area;
     use either::Either;
-        
+
     fn dotest(seq: &[Either<(f64, f64), f64>], expected: &[Either<(f64, f64), f64>]) {
         let actual = sort_by_area(seq);
-        assert!(actual == expected, "With seq = {seq:?}\nExpected {expected:?} but got {actual:?}")
+        assert!(
+            actual == expected,
+            "With seq = {seq:?}\nExpected {expected:?} but got {actual:?}"
+        )
     }
 
     #[test]
     fn fixed_tests() {
-        dotest(&[Either::Left((4.23, 6.43)), Either::Right(1.23), Either::Right(3.444), Either::Left((1.342, 3.212))], &[Either::Left((1.342, 3.212)), Either::Right(1.23), Either::Left((4.23, 6.43)), Either::Right(3.444)]);
-        // dotest(&[Either::Left((2.0, 5.0)), Either::Right(6.0)], &[Either::Left((2.0, 5.0)), Either::Right(6.0) ]);
-        // dotest(&[], &[]);
+        dotest(
+            &[
+                Either::Left((4.23, 6.43)),
+                Either::Right(1.23),
+                Either::Right(3.444),
+                Either::Left((1.342, 3.212)),
+            ],
+            &[
+                Either::Left((1.342, 3.212)),
+                Either::Right(1.23),
+                Either::Left((4.23, 6.43)),
+                Either::Right(3.444),
+            ],
+        );
+        dotest(&[Either::Left((2.0, 5.0)), Either::Right(6.0)], &[Either::Left((2.0, 5.0)), Either::Right(6.0) ]);
+        dotest(&[], &[]);
     }
 }
