eb0b5e96e723aa5adf85f8259c9e20e72439891f
Jay T <jaytula@gmail.com>
2023-01-13 07:29:43 -0800

    Complete 'Robinson Crusoe'

--- src/robinson_crusoe.rs
+++ src/robinson_crusoe.rs
@@ -1,23 +1,44 @@
-// https://www.codewars.com/kata/5d95b7644a336600271f52ba/train/rust
+// https://www.codewars.com/kata/5d95b7644a336600271f52ba/solutions/rust
 
-use std::f64;
+use std::f64::{self, consts::PI};
 
 fn crusoe(n: i32, d: f64, ang: i32, distmult: f64, angmult: f64) -> (f64, f64) {
-    // your code
+    let mut x = 0.0;
+    let mut y = 0.0;
+    let mut ang = ang as f64;
+    let mut d = d;
+
+    for _ in 0..n {
+        x = x + d * (ang as f64 * PI / 180.0).cos();
+        y = y + d * (ang as f64 * PI / 180.0).sin();
+        ang *= angmult;
+        d *= distmult;
+    }
+    (x, y)
 }
 
 #[cfg(test)]
-    mod tests {
+mod tests {
     use super::*;
     use float_eq::float_eq;
 
     fn dotest(n: i32, d: f64, ang: i32, distmult: f64, angmult: f64, expect: (f64, f64)) -> () {
         let merr = 1.0e-9;
         let actual = crusoe(n, d, ang, distmult, angmult);
-        let res0 = float_eq!(actual.0, expect.0, abs <= merr) || float_eq!(actual.0, expect.0, rmax <= merr);
-        let res1 = float_eq!(actual.1, expect.1, abs <= merr) || float_eq!(actual.1, expect.1, rmax <= merr);
-        assert!(res0, "Expected.0 value must be near: {:e} but was:{:e}", expect.0, actual.0);
-        assert!(res1, "Expected.1 value must be near: {:e} but was:{:e}", expect.1, actual.1);
+        let res0 = float_eq!(actual.0, expect.0, abs <= merr)
+            || float_eq!(actual.0, expect.0, rmax <= merr);
+        let res1 = float_eq!(actual.1, expect.1, abs <= merr)
+            || float_eq!(actual.1, expect.1, rmax <= merr);
+        assert!(
+            res0,
+            "Expected.0 value must be near: {:e} but was:{:e}",
+            expect.0, actual.0
+        );
+        assert!(
+            res1,
+            "Expected.1 value must be near: {:e} but was:{:e}",
+            expect.1, actual.1
+        );
     }
 
     #[test]
@@ -25,7 +46,5 @@ fn crusoe(n: i32, d: f64, ang: i32, distmult: f64, angmult: f64) -> (f64, f64) {
         dotest(8, 0.22, 3, 1.01, 1.15, (1.814652098870, 0.164646220964));
         dotest(29, 0.13, 21, 1.01, 1.09, (0.318341393410, 2.292862212314));
         dotest(45, 0.10, 3, 1.01, 1.10, (2.689897523779, 2.477953232467));
-        
     }
 }
-
